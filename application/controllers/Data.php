c632fd13609416a4b692d0bbc8034ac1938f5a2d
alffz <alfriansyah0408@gmail.com>
2021-12-10 20:35:43 +0700

    hapus datatable di controller data

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -38,28 +38,5 @@
             $this->load->view('data/view_nomor_rumah_copy');
             $this->load->view('footer');
         }
-
-        function get_nomor_rumah() 
-    {
-        $list = $this->model_nomor_rumah->get_datatables();
-        $data = array();
-        $no = $_POST['start'];
-        foreach ($list as $field) {
-            $no++;
-            $row = array();
-            $row[] = $no;
-            $row[] = $field->nomor_rumah;
- 
-            $data[] = $row;
-        }
- 
-        $output = array(
-            "draw" => $_POST['draw'],
-            "recordsTotal" => $this->model_nomor_rumah->count_all(),
-            "recordsFiltered" => $this->model_nomor_rumah->count_filtered(),
-            "data" => $data,
-        );
-        //output dalam format JSON
-        echo json_encode($output);
-    }
+        
     }
