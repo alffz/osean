441ba31d9874368f690e7efb0ee6cf695042222a
alffz <alfriansyah0408@gmail.com>
2021-12-14 21:46:40 +0700

    model nomor rumah

--- application/models/data/model_nomor_rumah.php
+++ application/models/data/model_nomor_rumah.php
@@ -1,67 +1,70 @@
 <?php
 
-    class model_nomor_rumah extends CI_Model{
+class model_nomor_rumah extends CI_Model
+{
     var $table = 'nomor_rumah';
     var $column_order = array(null, 'nomor_rumah'); //field yang ada di table user
     var $column_search = array('nomor_rumah'); //field yang diizin untuk pencarian 
     var $order = array('nomor_rumah' => 'asc'); // default order  
-    
+
+    // nomor rumah untuk loop
+    public function nomor_rumah()
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        return $this->db->get()->result();
+    }
+
     private function _get_datatables_query()
     {
         // select data
         $this->db->select('*');
-        $this->db->from($this->table); 
-        $i = 0;     
-        foreach ($this->column_search as $item) {// looping awal        
-            if(@$_POST['search']['value']){ // jika datatable mengrimkan pencarian dengan metode POST 
-                if($i===0) // looping awal
+        $this->db->from($this->table);
+
+        $i = 0;
+        foreach ($this->column_search as $item) { // looping awal        
+            if (@$_POST['search']['value']) { // jika datatable mengrimkan pencarian dengan metode POST 
+                if ($i === 0) // looping awal
                 {
-                    $this->db->group_start(); 
+                    $this->db->group_start();
                     $this->db->like($item, $_POST['search']['value']);
-                }
-                else
-                {
+                } else {
                     $this->db->or_like($item, $_POST['search']['value']);
                 }
- 
-                if(count($this->column_search) - 1 == $i) 
-                    $this->db->group_end(); 
+
+                if (count($this->column_search) - 1 == $i)
+                    $this->db->group_end();
             }
             $i++;
         }
-         
-        if(isset($_POST['order'])) 
-        {
+
+        if (isset($_POST['order'])) {
             $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
-        } 
-        else if(isset($this->order))
-        {
+        } else if (isset($this->order)) {
             $order = $this->order;
             $this->db->order_by(key($order), $order[key($order)]);
         }
     }
- 
+
     function get_datatables()
     {
         $this->_get_datatables_query();
-        if($_POST['length'] != -1)
-        $this->db->limit($_POST['length'],$_POST['start']);
+        if ($_POST['length'] != -1)
+            $this->db->limit($_POST['length'], $_POST['start']);
         $query = $this->db->get();
         return $query->result();
     }
- 
+
     function count_filtered()
     {
         $this->_get_datatables_query();
         $query = $this->db->get();
         return $query->num_rows();
     }
- 
+
     public function count_all()
     {
         $this->db->from($this->table);
         return $this->db->count_all_results();
     }
- 
-    }
-    
+}
