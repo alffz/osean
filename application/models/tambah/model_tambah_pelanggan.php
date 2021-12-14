28363b43697d5f2baf6a2a26bf718946c2841d9f
alffz <alfriansyah0408@gmail.com>
2021-12-14 21:47:02 +0700

    model tambah pelanggan

--- application/models/tambah/model_tambah_pelanggan.php
+++ application/models/tambah/model_tambah_pelanggan.php
@@ -1,17 +1,27 @@
 <?php
 
-    class model_tambah_pelanggan extends CI_Model {
+class model_tambah_pelanggan extends CI_Model
+{
 
-        
-        public function insert_pelanggan() {
-            // add a new item
-            $data = array(
-                'nama_pelanggan' => $this->input->post('nama_pelanggan'),
-                'alamat' => $this->input->post('alamat'),
-                'no_telp' => $this->input->post('no_telp'),
-                'email' => $this->input->post('email'),
-                'username' => $this->input->post('username'),
-                'password' => $this->input->post('password')
-            );
-        }
 
+    public function insert_pelanggan()
+    {
+        // add a new item
+        $data = array(
+            'id_pelanggan'        => uniqid(),
+            'id_anggota_keliling' => $this->input->post('anggota', true),
+            'id_gang'             => $this->input->post('gang', true),
+            'nama_pelanggan'      => $this->input->post('nama_pelanggan', true),
+            'id_nomor_rumah'      => $this->input->post('nomor_rumah', true),
+            'nomor_hp'            => $this->input->post('nomor_hp'),
+            'harga'               => $this->input->post('harga', true),
+            'jumlah_galon'        => $this->input->post('galon', true),
+            'jumlah_keluarga'     => $this->input->post('anggota_keluarga', true),
+            'status_rumah'        => $this->input->post('status_rumah', true),
+            'harga'               => $this->input->post('harga', true),
+            'is_langganan_active' => 1,
+        );
+        // insert
+        $this->db->insert('pelanggan', $data);
+    }
+}
