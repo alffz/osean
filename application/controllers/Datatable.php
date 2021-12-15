b5729b8f7d1ab84206e50b26cd610d261ee61cc5
alffz <alfriansyah0408@gmail.com>
2021-12-15 11:18:58 +0700

    fix datatable pelanggan

--- application/controllers/Datatable.php
+++ application/controllers/Datatable.php
@@ -44,26 +44,25 @@ class Datatable extends CI_Controller
             $no++;
             $row = array();
             $row[] = $no;
-            $row[] = $field->id_anggota_keliling;
-            $row[] = $field->id_gang;
+            $row[] = $field->nama_anggota_keliling;
+            $row[] = $field->nama_gang;
             $row[] = $field->nama_pelanggan;
-            $row[] = $field->id_nomor_rumah;
+            $row[] = $field->nomor_rumah;
             $row[] = $field->nomor_hp;
             $row[] = $field->harga;
             $row[] = $field->jumlah_galon;
             $row[] = $field->jumlah_keluarga;
             $row[] = $field->status_rumah;
             // anchor
-            $row[] = '<a class="btn btn-sm btn-primary" href="javascript:void(0)" title="Edit" onclick="edit_pelanggan(' . "'" . $field->id_pelanggan . "'" . ')"><i class="glyphicon glyphicon-pencil"></i> Edit</a>
-                  <a class="btn btn-sm btn-danger" href="javascript:void(0)" title="Hapus" onclick="delete_pelanggan(' . "'" . $field->id_pelanggan . "'" . ')"><i class="glyphicon glyphicon-trash"></i> Delete</a>';
+            $row[] = '<a class="btn btn-sm btn-primary" href="../edit/pelanggan/' . $field->id_pelanggan . '"><i class="glyphicon glyphicon-pencil"></i> Edit</a>';
 
             $data[] = $row;
         }
 
         $output = array(
             "draw" => $_POST['draw'],
-            "recordsTotal" => $this->model_nomor_rumah->count_all(),
-            "recordsFiltered" => $this->model_nomor_rumah->count_filtered(),
+            "recordsTotal" => $this->model_data_pelanggan->count_all(),
+            "recordsFiltered" => $this->model_data_pelanggan->count_filtered(),
             "data" => $data,
         );
         //output dalam format JSON
