4aeeba40be349b63d601e2fd93b3f4d8bd5e16a2
alffz <alfriansyah0408@gmail.com>
2021-12-15 10:53:11 +0700

    lower case nama model

--- application/models/data/model_data_gang.php
+++ application/models/data/model_data_gang.php
@@ -1,11 +1,11 @@
 <?php
 
-    class Model_Data_Gang extends CI_Model{
-    
-        
-        // get all gangs
-        public function list_gang(){
-            $query = $this->db->get('gang');
-            return $query->result();
-        }
+class model_data_gang extends CI_Model
+{
+    // get all gangs
+    public function list_gang()
+    {
+        $query = $this->db->get('gang');
+        return $query->result();
     }
+}
