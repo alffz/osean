dc51da3fa6aecdf8b277cb1220c210d0a2c0cdb9
alffz <alfriansyah0408@gmail.com>
2021-12-20 11:22:01 +0700

    $data['user_loged'] for sidebar

--- application/controllers/Anggota.php
+++ application/controllers/Anggota.php
@@ -1,22 +1,28 @@
 <?php
 
-    class Anggota extends CI_Controller {
+class Anggota extends CI_Controller
+{
 
-        public function __construct() {
-            parent::__construct();
-            $this->load->model('anggota/anggota_model');
-            $this->load->library('form_validation');
-            $this->load->library('pagination');
-        }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('anggota/anggota_model');
+        $this->load->library('form_validation');
+        $this->load->library('pagination');
+    }
 
     // show data with pagination
-        public function index() {
+    public function index()
+    {
 
-            // get data from model
-            $data['anggota'] = $this->anggota_model->get_data_anggota();
-            $this->load->view('header');
-            $this->load->view('sidebar');
-            $this->load->view('anggota/view_anggota',$data);
-            $this->load->view('footer');
-        }
+        // get data from model
+        $data   = [
+            'user_loged' => $this->model_data_user->get_user_by_session(),
+            'anggota'    => $this->anggota_model->get_data_anggota(),
+        ];
+        $this->load->view('header');
+        $this->load->view('sidebar', $data);
+        $this->load->view('anggota/view_anggota', $data);
+        $this->load->view('footer');
     }
+}
