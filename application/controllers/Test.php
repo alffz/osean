60a7448916554287a206466ea80bdd453d5556c7
alffz <alfriansyah0408@gmail.com>
2021-12-20 11:26:30 +0700

    controller test

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,12 +1,14 @@
 <?php
-    defined('BASEPATH') OR exit('No direct script access allowed');
-    class Test extends CI_Controller {
-        public function index() {
-            $this->load->view('header');
-            $this->load->view('sidebar');
-            $this->load->view('view_test');
-            $this->load->view('footer');
-        }
+defined('BASEPATH') or exit('No direct script access allowed');
+class Test extends CI_Controller
+{
+    // constructor
+    public function __construct()
+    {
+        parent::__construct();
     }
-
-?>
+    public function index()
+    {
+        $this->load->view('view_test');
+    }
+}
