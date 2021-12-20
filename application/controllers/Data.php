e18a1b397142c068c283baffb7ac19f47c8a9b39
alffz <alfriansyah0408@gmail.com>
2021-12-20 11:22:10 +0700

    $data['user_loged'] for sidebar

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -19,9 +19,12 @@ class Data extends CI_Controller
 
     public function gang()
     {
-        $data['gang'] = $this->model_data_gang->list_gang();
+        $data   = [
+            'user_loged' => $this->model_data_user->get_user_by_session(),
+            'gang'       => $this->model_data_gang->list_gang(),
+        ];
         $this->load->view('header');
-        $this->load->view('sidebar');
+        $this->load->view('sidebar', $data);
         $this->load->view('data/view_gang', $data);
         $this->load->view('footer');
     }
@@ -29,16 +32,22 @@ class Data extends CI_Controller
     // data pelanggan
     public function pelanggan()
     {
+        $data   = [
+            'user_loged' => $this->model_data_user->get_user_by_session(),
+        ];
         $this->load->view('header');
-        $this->load->view('sidebar');
+        $this->load->view('sidebar', $data);
         $this->load->view('data/view_pelanggan');
         $this->load->view('footer');
     }
 
     public function rumah()
     {
+        $data   = [
+            'user_loged' => $this->model_data_user->get_user_by_session(),
+        ];
         $this->load->view('header');
-        $this->load->view('sidebar');
+        $this->load->view('sidebar', $data);
         $this->load->view('data/view_nomor_rumah_copy');
         $this->load->view('footer');
     }
@@ -46,8 +55,11 @@ class Data extends CI_Controller
     // anggota keliling
     public function anggota()
     {
+        $data   = [
+            'user_loged' => $this->model_data_user->get_user_by_session(),
+        ];
         $this->load->view('header');
-        $this->load->view('sidebar');
+        $this->load->view('sidebar', $data);
         $this->load->view('data/view_anggota_keliling');
         $this->load->view('footer');
     }
